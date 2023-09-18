3bc514da5f61575812695d0aca6a5abdea458c26
Ado1871 <andrijapbrzi@gmail.com>
2023-09-18 20:14:17 +0200

    initial push

---
+++ src/main/java/ch/bbw/zork/RoomWithEnemies.java
@@ -0,0 +1,26 @@
+package ch.bbw.zork;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomWithEnemies extends Room {
+
+    private List<Enemy> enemies;
+
+    public RoomWithEnemies(String description) {
+        super(description);
+        this.enemies = new ArrayList<>();
+    }
+
+    public void addEnemy(Enemy enemy) {
+        enemies.add(enemy);
+    }
+
+    public void removeEnemy(Enemy enemy) {
+        enemies.remove(enemy);
+    }
+
+    public List<Enemy> getEnemies() {
+        return enemies;
+    }
+}
